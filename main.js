cda0166532465990f46a402a59e5b0b9dd43e3c6
Adam Noffsinger <adamnoffsinger@Adams-MacBook-Pro-M1-Max.local>
2023-05-18 08:57:20 -0700

    Refined 3d phone model

--- main.js
+++ main.js
@@ -3,9 +3,14 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const loader = new GLTFLoader();
 
-loader.load( 'public/mbp.gltf', function ( gltf ) {
+loader.load( 'public/phone.gltf', function ( gltf ) {
 
-	scene.add( gltf.scene );
+	
+
+    const model = gltf.scene.children[0];
+    // model.rotation.z = -0.5;
+    // model.rotation.x = -0.5;
+    scene.add(model);
 
 }, undefined, function ( error ) {
 
@@ -15,17 +20,25 @@ loader.load( 'public/mbp.gltf', function ( gltf ) {
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+
+
+camera.position.set(-5, 0, 0)
+
+renderer.domElement.style.position = 'fixed';
+renderer.domElement.style.top = '0';
+renderer.domElement.style.right = '0';
+
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+camera.position.z = 10;
 
 function animate() {
 	requestAnimationFrame( animate );
